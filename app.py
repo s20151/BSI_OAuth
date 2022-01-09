b2c3cd7702f70c06a4edab540820cdb7cc2f3429
s20151 <s20151@pjwstk.edu.pl>
2022-01-09 18:07:02 +0100

    added working logging form

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, session, abort, redirect
+import os, pathlib, requests
+from flask import Flask, session, abort, redirect, request
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-import os, pathlib
 
 client_secret_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 GOOGLE_CLIENT_ID = "887569292704-m92pcj08v19ua0i28j7gf5b5qepie0e7.apps.googleusercontent.com"
@@ -8,11 +11,11 @@ flow = Flow.from_client_secrets_file(client_secrets_file=client_secret_file,
                                      scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
                                      redirect_uri="http://127.0.0.1:5000/callback"
                                      )
-
-
 app = Flask("BSI Login App")
 app.secret_key = "testowy_tajny_klucz"
 
+# bypassing https requirement
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 @app.route("/")
 def index():
@@ -33,7 +36,25 @@ def logout():
 
 @app.route("/callback")
 def callback():
-    pass
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        return abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    return id_info
+    # session["google_id"] = id_info.get("sub")
+    # session["name"] = id_info.get("name")
+    # return redirect("/protected_area")
 
 
 def require_login(function):
@@ -45,7 +66,7 @@ def require_login(function):
 
     return wrapper
 
-c
+
 @app.route("/protected")
 @require_login
 def protected():
